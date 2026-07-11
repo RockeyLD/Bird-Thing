e9bac74e031bc1c7e5f0640e6485d8be7c92767c
RockeyLD <rl.longdragon@gmail.com>
2026-07-11 10:35:22 +0800

    feat: 用icons图片替换底部Tab栏和登录标识emoji

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -3,10 +3,10 @@ Component({
     selected: 0,
     safeAreaBottom: 0,
     list: [
-      { pagePath: '/pages/index/index', text: '首页', icon: '🏠' },
-      { pagePath: '/pages/library/library', text: '答题学鸟', icon: '📚' },
-      { pagePath: '/pages/codex/codex', text: '图鉴', icon: '📖' },
-      { pagePath: '/pages/pet/pet', text: '宠物养成', icon: '🐣' }
+      { pagePath: '/pages/index/index', text: '首页', iconPath: '/images/icons/主页.png', selectedIconPath: '/images/icons/主页.png' },
+      { pagePath: '/pages/library/library', text: '答题学鸟', iconPath: '/images/icons/答题学鸟.png', selectedIconPath: '/images/icons/答题学鸟.png' },
+      { pagePath: '/pages/codex/codex', text: '图鉴', iconPath: '/images/icons/图鉴.png', selectedIconPath: '/images/icons/图鉴.png' },
+      { pagePath: '/pages/pet/pet', text: '宠物养成', iconPath: '/images/icons/宠物养成.png', selectedIconPath: '/images/icons/宠物养成.png' }
     ]
   },
   lifetimes: {
